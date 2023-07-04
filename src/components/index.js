ed8b038fec0b47ad748717b278480e642f6c5a2b
stel0 <86563248+stel0@users.noreply.github.com>
2023-07-04 16:30:06 -0400

    firs commit

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export{default as NavBar } from './Navbar';
+export{default as ChannelDetail } from './ChannelDetail';
+export{default as Feed } from './Feed';
+export{default as SearchFeed } from './SearchFeed';
+export { default as VideoDetail } from "./VideoDetail";
