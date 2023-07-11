498743e54e4797746c5d1a299e442656dad90407
stel0 <86563248+stel0@users.noreply.github.com>
2023-07-11 09:06:02 -0400

    fixing the comunication of the api

--- site/src/api/video.api.js
+++ site/src/api/video.api.js
@@ -1,18 +1,10 @@
 import axios from 'axios'
 
 const videoApi = axios.create({
-  baseURL: 'http://127.0.0.1:8000/videos/api/v1/videos/',
-  headers:{
-    "Content-Type": "multipart/form-data",
-  }
+  baseURL: "http://127.0.0.1:8000/videos/api/v1/videos/",
+  headers: {"Content-Type": "multipart/form-data"}
 });
 
 
 export const getAllVideos = () =>  videoApi.get('/');
-export const uploadVideo = (video) => videoApi.post('/',video)
-  .then(response=>{
-    console.log(response)
-  })
-  .catch(error => {
-    console.log(error)
-  })
+export const uploadVideo = (data) => videoApi.post('/',data);
--- site/src/pages/VideoFormPage.jsx
+++ site/src/pages/VideoFormPage.jsx
@@ -1,34 +1,40 @@
-import {useForm} from 'react-hook-form'
+import { useForm } from "react-hook-form";
 import { uploadVideo } from "../api/video.api";
-import {AppContext} from '../context/AppContext'
-import {useContext,useState} from 'react'
+import { AppContext } from "../context/AppContext";
+import { useContext, useState } from "react";
 
 export function VideoFormPage() {
-  {/*loadVideos: para actualizar la lista de videos*/}
+  {
+    /*loadVideos: para actualizar la lista de videos*/
+  }
   const [fileName, setFileName] = useState(null);
 
   const { loadVideos } = useContext(AppContext);
 
-  const {register,handleSubmit,formState:{errors}} = useForm();
-  
-  {/*se ejecuta esta funcion al enviar el formulario*/}
-  const onsub = handleSubmit(async data=>{
-    // const video = {
-    //   title: await data.title,
-    //   file_video: await data.file_video[0].name,
-    // };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-    let formData = new FormData();
-    formData.append("video",fileName);
-    {/*se cargan los datos en la base de datos*/}
-    const res = await uploadVideo(formData);
-    {/*actualiza la base*/}
+  {
+    /*se ejecuta esta funcion al enviar el formulario*/
+  }
+  const onsub = async (data) => {
+    {
+      /*se cargan los datos en la base de datos*/
+    }
+    const res = await uploadVideo(data);
+    console.log(res);
+    {
+      /*actualiza la base*/
+    }
     loadVideos();
-  })
+  };
 
   return (
     <div>
-      <form onSubmit={onsub}>
+      <form onSubmit={handleSubmit(onsub)}>
         <input
           type="text"
           placeholder="title"
@@ -36,9 +42,7 @@ export function VideoFormPage() {
         />
         {errors.title && <span>This field is required</span>}
 
-        <input type="file" {...register("file_video", { required: true })} onChange={e=>{
-          setFileName(e.target.files[0])
-        }} />
+        <input type="file" {...register("file_video", { required: true })} />
         {errors.file_video && <span>This field is required</span>}
 
         <button>Save</button>
